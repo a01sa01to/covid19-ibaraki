4ad5b7c8f544854c0e2e930c13127b68ea858c95
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-03-24 17:19:57 +0900

    Update default.vue  - Imageのsrcを変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -143,7 +143,7 @@ export default Vue.extend({
         {
           hid: 'og:image',
           property: 'og:image',
-          content: this.$tc('ogp.og:image')
+          content: 'https://covid19-ibaraki.netlify.com/ss.png'
         },
         {
           hid: 'apple-mobile-web-app-title',
@@ -158,7 +158,7 @@ export default Vue.extend({
         {
           hid: 'twitter:image',
           name: 'twitter:image',
-          content: this.$tc('ogp.og:image')
+          content: 'https://covid19-ibaraki.netlify.com/ss.png'
         }
       ]
     }
