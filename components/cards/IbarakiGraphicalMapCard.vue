68240d3f1f7a09f7dcda591e91087f2f0886c479
takonasu <piano88_viola_tako8@hotmail.co.jp>
2020-04-21 16:57:52 +0900

    add: 感染者数によって塗り分け

--- components/cards/IbarakiGraphicalMapCard.vue
+++ components/cards/IbarakiGraphicalMapCard.vue
@@ -9,6 +9,21 @@
         <p :class="$style.note">
           {{ $t('（注）退院している人数を含む') }}
         </p>
+        <p>凡例（単位は人）</p>
+        <table>
+          <tbody>
+            <tr>
+              <td><span class="color-test infected-level1" />1-5</td>
+              <td><span class="color-test infected-level2" />6-10</td>
+              <td><span class="color-test infected-level3" />11-15</td>
+            </tr>
+            <tr>
+              <td><span class="color-test infected-level4" />16-20</td>
+              <td><span class="color-test infected-level5" />21-30</td>
+              <td><span class="color-test infected-level6" />31以上</td>
+            </tr>
+          </tbody>
+        </table>
       </template>
       <ibaraki-map />
     </data-view>
@@ -41,14 +56,25 @@ export default {
         return x.居住地 === key.居住地
       }).length
     }
-
+    console.log(cityPatientsNumber)
     CityData.forEach((element) => {
-      if (cityPatientsNumber[element.city]) {
-        const targetElement = document.getElementById(
-          'ibaraki-map_svg__' + element.Romaji
-        )
-        targetElement.classList.add('level2')
+      if (!cityPatientsNumber[element.city]) {
+        return
       }
+      const targetElement = document.getElementById(
+        'ibaraki-map_svg__' + element.Romaji
+      )
+      if (cityPatientsNumber[element.city] <= 5)
+        targetElement.classList.add('infected-level1')
+      else if (cityPatientsNumber[element.city] <= 10)
+        targetElement.classList.add('infected-level2')
+      else if (cityPatientsNumber[element.city] <= 15)
+        targetElement.classList.add('infected-level3')
+      else if (cityPatientsNumber[element.city] <= 20)
+        targetElement.classList.add('infected-level4')
+      else if (cityPatientsNumber[element.city] <= 30)
+        targetElement.classList.add('infected-level5')
+      else targetElement.classList.add('infected-level6')
     })
   },
 }
@@ -64,10 +90,48 @@ export default {
 </style>
 <!-- 本来ならばSVGをinline展開してそこに限定してcssを適用するべきだが、inline展開ができなかったため妥協 -->
 <style lang="scss">
-.level1 {
-  fill: red !important;
+$infected-level1: #ccfbcc;
+$infected-level2: #88f2a9;
+$infected-level3: #44e5b7;
+$infected-level4: #00c1d5;
+$infected-level5: #004da5;
+$infected-level6: #000072;
+
+.color-test {
+  vertical-align: middle;
+  width: 2.5rem;
+  height: 1rem;
+  display: inline-block;
+  margin: 0 0.5rem 0 0;
 }
-.level2 {
-  fill: pink !important;
+// 1-5
+.infected-level1 {
+  fill: $infected-level1 !important;
+  background-color: $infected-level1;
+}
+// 6-10
+.infected-level2 {
+  fill: $infected-level2 !important;
+  background-color: $infected-level2;
+}
+// 10-15
+.infected-level3 {
+  fill: $infected-level3 !important;
+  background-color: $infected-level3;
+}
+// 15-20
+.infected-level4 {
+  fill: $infected-level4 !important;
+  background-color: $infected-level4;
+}
+// 21-30
+.infected-level5 {
+  fill: $infected-level5 !important;
+  background-color: $infected-level5;
+}
+// 31-
+.infected-level6 {
+  fill: $infected-level6 !important;
+  background-color: $infected-level6;
 }
 </style>
