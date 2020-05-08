9ebb353938ac004445b883696ddc14c82b7a013d
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-05-08 17:45:27 +0900

    Update: 「退院」についての文を変更

--- components/cards/DischargesCard.vue
+++ components/cards/DischargesCard.vue
@@ -8,7 +8,26 @@
       :date="Data.discharges_summary.date"
       :unit="$t('人')"
       :url="'https://www.pref.ibaraki.jp/1saigai/2019-ncov/taiin.html'"
-    />
+    >
+      <template v-slot:description>
+        <ul>
+          <li>
+            {{
+              $t(
+                '（注）「退院」には、宿泊療養施設から退所した方、自宅療養で陰性化した方を含む'
+              )
+            }}
+          </li>
+          <li>
+            {{
+              $t(
+                '（注）退院者数の把握には一定の期間を要しており、確認次第数値を更新している'
+              )
+            }}
+          </li>
+        </ul>
+      </template>
+    </time-bar-chart>
   </v-col>
 </template>
 
