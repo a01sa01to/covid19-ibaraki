3c71d055a1247191debe1077da14b37b9bc69884
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-05-10 00:24:02 +0900

    Fix: Error (WIP)

--- ogp_invalidator/install_dependencies.sh
+++ ogp_invalidator/install_dependencies.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-apt-get -y update && apt-get -y upgrade && apt-get install -y chromium-chromedriver python3-selenium
+apt-get -y update && apt-get -y upgrade && apt-get install -y chromium-chromedriver=81.0.4044.138 python3-selenium
