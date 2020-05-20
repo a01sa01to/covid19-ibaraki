7c106ea562e34663218324c9ca10e5ec621b0faa
a01sa01to <a01sa01to@users.noreply.github.com>
2020-05-20 10:07:49 +0000

    deploy: 9437a1ed27f64ddc36c0cd006fd894d1df1b850a

---
+++ workflow_tools/auto-sitemap/sitemap_dev.py
@@ -0,0 +1,77 @@
+# --- IMPORT --- #
+import os
+import glob
+import json
+import datetime
+
+# --- "sitemap" ディレクトリがなければ作成 --- #
+if not os.path.exists("sitemap"):
+  os.mkdir("sitemap")
+  print('Directory "sitemap" created')
+
+# --- 変数定義 --- #
+CHECK_DIR = ["data"]
+ENCODING = "UTF-8"
+JSON_FILES = ["data.json"]
+HOST = "https://covid19-dev.a01sa01to.com"
+data_json = {}
+posRate_json = {}
+
+# --- data.jsonを開く --- #
+with open("data/data.json", encoding=ENCODING) as file:
+  data_json = json.load(file)
+  print('Loaded data.json')
+
+# --- positive_rate.jsonを開く --- #
+with open("data/positive_rate.json", encoding=ENCODING) as file:
+  posRate_json = json.load(file)
+  print('Loaded positive_rate.json')
+
+# --- パスおよび変更日時の指定 --- #
+PATHS = {
+  "/": data_json["lastUpdate"],
+  "/cards/details-of-confirmed-cases": data_json["main_summary"]["children"][0]["date"],
+  "/cards/number-of-confirmed-cases": data_json["patients"]["date"],
+  "/cards/attributes-of-confirmed-cases": data_json["patients"]["date"],
+  "/cards/number-of-reports-to-covid19-telephone-advisory-center": data_json["contacts"]["date"],
+  "/cards/number-of-inspection-persons": data_json["inspection_persons"]["date"],
+  "/cards/ibaraki-city-table": data_json["patients"]["date"],
+  "/cards/ibaraki-city-map-table": data_json["patients"]["date"],
+  "/cards/number-of-recovered": data_json["recovered_summary"]["date"],
+  "/cards/number-of-deaths": data_json["deaths_summary"]["date"],
+  "/cards/ibaraki-colona-next": data_json["ibk_colona_next"]["date"],
+  "/cards/positive-rate": posRate_json["date"],
+  "/about": "2020/05/15 18:00",
+  "/contacts": "2020/05/15 18:00",
+  "/flow": "2020/05/15 18:00",
+  "/helpus": "2020/05/15 18:00",
+  "/otherpref": "2020/05/15 18:00",
+  "/parent": "2020/05/15 18:00",
+  "/worker": "2020/05/15 18:00"
+}
+
+# --- Sitemap.xmlの作成部分 --- #
+for lang in ("ja", "en", "ja-basic"):
+  print('----------')
+  print("Lang: {}".format(lang))
+  # --- なければ作成 --- #
+  if not os.path.exists("sitemap/{}".format(lang)):
+    os.mkdir("sitemap/{}".format(lang))
+    print('Language directory created')
+  # --- なければ自動生成される。あればそれを開く --- #
+  with open("sitemap/{}/sitemap.xml".format(lang), mode="a", encoding=ENCODING) as result:
+    result.write('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:xhtml="http://www.w3.org/1999/xhtml">')
+    for path, updatedAt in PATHS.items():
+      if lang == "ja":
+        result.write('<url><loc>{}{}</loc>'.format(HOST,path))
+      else:
+        result.write('<url><loc>{}/{}{}</loc>'.format(HOST, lang, path))
+      result.write('<lastmod>{}</lastmod>'.format(datetime.datetime.strptime(updatedAt, "%Y/%m/%d %H:%M").strftime("%Y-%m-%d")))
+      result.write('<changefreq>daily</changefreq>')
+      result.write('<mobile:mobile />')
+      result.write('</url>')
+      print("Path: {} (Updated at {}) ...done!".format(path, updatedAt))
+    result.write('</urlset>')
+
+print('----------')
+print('All done!')
---
+++ workflow_tools/auto-tweet/tweet.py
@@ -0,0 +1,56 @@
+import twitter
+import os
+import glob
+import json
+
+auth = twitter.OAuth(consumer_key=os.environ['API_KEY'],
+consumer_secret=os.environ['API_SECRET'],
+token=os.environ['ACCESS_TOKEN'],
+token_secret=os.environ['ACCESS_SECRET'])
+
+CHECK_DIR = ["data"]
+ENCODING = "UTF-8"
+JSON_FILES = ["data.json"]
+lastUpdate = ""
+
+t = twitter.Twitter(auth=auth)
+
+for cdir in CHECK_DIR:
+  cdir = os.path.join(os.pardir, os.pardir, cdir)
+
+  json_files = glob.glob(cdir + os.sep + "**" + os.sep + "*.json", recursive=True)
+  # 各jsonファイルについて処理
+  for path in json_files:
+    file_name = os.path.basename(path)
+    # jsonファイルが調べるべきjsonであるか
+    if file_name in JSON_FILES:
+      with open(path, encoding=ENCODING) as file:
+        # jsonを読み込み
+        json_content = json.load(file)
+        # タグリストを生成
+        if file_name == JSON_FILES[0]:  # data.jsonの場合
+          lastUpdate = json_content["lastUpdate"]
+          print("更新日時: "+lastUpdate+"\n")
+
+msg = str(os.environ['MSG'])
+msgJson = json.loads(msg)
+commitMsg = msgJson[len(msgJson)-1]["message"].splitlines()
+print("Commit Message:\n----------")
+print(msg)
+print("----------\n")
+
+updContent = ""
+for i in range(len(commitMsg)):
+  if i<1: continue
+  txt = commitMsg[i]
+  updContent += txt
+  updContent += "\n"
+
+print("LAST COMMIT MESSAGE")
+print(updContent)
+
+status="【 #茨城県 版 #新型コロナウイルス 対策サイト 更新情報】\nhttps://ibaraki.stopcovid19.jp/\n"+lastUpdate+"更新\n\n更新内容\n"+updContent+"\n#StayHome #家で過ごそう #コロナに負けるな #ワンチーム茨城"
+
+print("TWEET MESSAGE\n----------\n"+status+"\n----------")
+
+t.statuses.update(status=status)
