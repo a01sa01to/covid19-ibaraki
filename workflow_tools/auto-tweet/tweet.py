86fc41ed92cbcc77f73de9d636071e5bcfdbfa0f
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-07-08 18:32:47 +0900

    Update tweet.py

--- workflow_tools/auto-tweet/tweet.py
+++ workflow_tools/auto-tweet/tweet.py
@@ -39,7 +39,7 @@ for i in range(len(commitMsg)):
 print("LAST COMMIT MESSAGE")
 print(updContent)
 
-status="【 #茨城県 版 #新型コロナウイルス 対策サイト 更新情報】\nhttps://ibaraki.stopcovid19.jp/\n"+lastUpdate+"更新\n\n更新内容\n"+updContent+"\n#StayHome #家で過ごそう #コロナに負けるな #ワンチーム茨城"
+status="【 #茨城県 版 #新型コロナウイルス 対策サイト 更新情報】\nhttps://ibaraki.stopcovid19.jp/\n"+lastUpdate+"更新\n\n更新内容\n"+updContent+"\n#いばらきアマビエちゃん #コロナに負けるな #ワンチーム茨城"
 
 print("TWEET MESSAGE\n----------\n"+status+"\n----------")
 
