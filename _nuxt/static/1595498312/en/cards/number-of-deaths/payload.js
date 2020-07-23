6eeb949cc0de3b48b54fb4b586bde21da78b2426
a01sa01to <a01sa01to@users.noreply.github.com>
2020-07-23 09:58:44 +0000

    deploy: 2827da750d67c63efe1df7377c9aeb0541956299

---
+++ _nuxt/static/1595498312/en/cards/number-of-deaths/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/en/cards/number-of-deaths", {data:[{}],fetch:[],mutations:void 0});
---
+++ _nuxt/static/1595498312/ja-basic/cards/ibaraki-corona-next/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/ja-basic/cards/ibaraki-corona-next", {data:[{}],fetch:[],mutations:void 0});
