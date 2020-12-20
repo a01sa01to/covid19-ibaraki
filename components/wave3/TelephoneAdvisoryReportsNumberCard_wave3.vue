ced2d61ce4b1730bacdec4726bfa8ac8111ae130
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-12-20 18:09:18 +0900

    Fix: 第2波 -> 第3波

--- components/wave3/TelephoneAdvisoryReportsNumberCard_wave3.vue
+++ components/wave3/TelephoneAdvisoryReportsNumberCard_wave3.vue
@@ -2,9 +2,9 @@
   <v-col cols="12" md="6" class="DataCard">
     <client-only>
       <time-bar-chart
-        :title="$t('受診相談窓口における相談件数（第2波）')"
-        :title-id="'wave2/number-of-reports-to-covid19-telephone-advisory-center'"
-        :chart-id="'wave2-time-bar-chart-contacts'"
+        :title="$t('受診相談窓口における相談件数（第3波）')"
+        :title-id="'wave3/number-of-reports-to-covid19-telephone-advisory-center'"
+        :chart-id="'wave3-time-bar-chart-contacts'"
         :chart-data="contactsGraph"
         :date="date"
         :unit="$t('件')"
