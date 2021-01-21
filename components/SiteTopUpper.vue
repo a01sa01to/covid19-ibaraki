6e96ba83f63a917da9a2fcfa69cbece4604000a3
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-01-21 19:55:35 +0900

    Update SiteTopUpper.vue

--- components/SiteTopUpper.vue
+++ components/SiteTopUpper.vue
@@ -17,6 +17,13 @@
     </div>
     <whats-new class="mb-4" :items="newsItems" :is-emergency="true" />
     <lazy-static-info
+      v-if="$vuetify.breakpoint.smAndUp"
+      class="mb-4"
+      :url="localePath('/EmergencyMeasure')"
+      :text="$t('茨城県独自の緊急事態宣言が発令されています')"
+      :btn-text="$t('緊急事態宣言について')"
+    />
+    <lazy-static-info
       v-if="$vuetify.breakpoint.smAndUp || showStaticInfo"
       class="mb-4"
       :url="'https://www.pref.ibaraki.jp/hokenfukushi/yobo/kiki/yobo/kansen/idwr/information/other/documents/corona-soudan.html'"
