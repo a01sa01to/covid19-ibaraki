a7e40632fcbdce2c8d27f1ba762100ec837af1f7
a01sa01to <a01sa01to@users.noreply.github.com>
2021-04-29 10:17:07 +0000

    deploy: 0fbd6dd426effd397ae03acad03bcc8347bfaf68

--- _nuxt/static/1619684866/cards/ibaraki-graphical-map/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/cards/ibaraki-graphical-map", {data:[{}],fetch:{},mutations:void 0});
--- _nuxt/static/1619684866/ja-basic/cards/infection-status-by-national-index/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/ja-basic/cards/infection-status-by-national-index", {data:[{}],fetch:{},mutations:void 0});
