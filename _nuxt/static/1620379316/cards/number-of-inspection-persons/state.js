4976b353283c3849dd18a64dcb1a3376e20f6e92
a01sa01to <a01sa01to@users.noreply.github.com>
2021-05-08 11:37:31 +0000

    deploy: e825675e6fc211207d1d8ad0fc38cb6bfa65a1fe

--- _nuxt/static/1620379316/cards/number-of-inspection-persons/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620379316",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}},__i18n:{langs:{}}}}("\u002Fcards\u002Fnumber-of-inspection-persons\u002F"));
