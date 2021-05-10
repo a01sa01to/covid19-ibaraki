c70247111b240d82f78b7e1ddc87d549c445ef8c
a01sa01to <a01sa01to@users.noreply.github.com>
2021-05-10 07:21:03 +0000

    deploy: cba57f831e9b5ef59027304d8b060c8d279a4ab6

--- _nuxt/static/1620629816/cards/number-of-deaths/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620629816",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}},__i18n:{langs:{}}}}("\u002Fcards\u002Fnumber-of-deaths\u002F"));
