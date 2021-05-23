54bcf3242012425e0bc80e9fd3243399024655b7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-05-23 15:49:24 +0900

    Update dataUpdate.yml: 不要なRenameを削除

--- workflow_tools/csv2json/test_people.py
+++ workflow_tools/csv2json/test_people.py
@@ -2,7 +2,7 @@ import csv
 import json
 
 json_list = {"labels":[],"datasets":[{"label":"検査実施人数","data":[]}]}
-filename = "080004_ibaraki_covid19_test_people"
+filename = "test_people"
 
 # CSV読み込み
 with open('{}.csv'.format(filename), 'r', encoding="UTF-8") as f:
