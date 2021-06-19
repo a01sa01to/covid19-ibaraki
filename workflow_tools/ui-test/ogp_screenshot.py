8ea071c71a5533ff09e11f96f9895aa105886a1c
a01sa01to <a01sa01to@users.noreply.github.com>
2021-06-19 04:17:51 +0000

    deploy: 769fff37391c64c8beef980336ec31590f4a9dda

--- workflow_tools/ui-test/ogp_screenshot.py
+++ workflow_tools/ui-test/ogp_screenshot.py
@@ -7,24 +7,24 @@ if not os.path.exists("ogp"):
   os.mkdir("ogp")
 
 PATHS = [
-  "/cards/details-of-confirmed-cases",
-  "/cards/ibaraki-graphical-map"
-  "/cards/ibaraki-corona-next",
-  "/cards/number-of-tested",
-  "/cards/number-of-confirmed-cases",
-  "/cards/number-of-mutant-confirmed-cases",
-  "/cards/attributes-of-confirmed-cases",
-  "/cards/number-of-reports-to-covid19-telephone-advisory-center",
-  "/cards/number-of-inspection-persons",
-  "/cards/number-of-mutant-inspection-persons",
-  "/cards/number-of-confirmed-cases-by-municipalities",
-  "/cards/number-of-recovered",
-  "/cards/number-of-deaths",
-  "/cards/number-of-confirmed-cases-by-age",
-  "/cards/untracked-rate",
-  "/cards/infection-status-by-national-index"
-  "/cards/vaccination-1st",
-  "/cards/vaccination-2nd",
+  'cards/attributes-of-confirmed-cases',
+  'cards/number-of-confirmed-cases-by-age',
+  'cards/number-of-confirmed-cases-by-municipalities',
+  'cards/details-of-confirmed-cases',
+  'cards/number-of-confirmed-cases',
+  'cards/number-of-deaths',
+  'cards/ibaraki-corona-next',
+  'cards/ibaraki-graphical-map',
+  'cards/number-of-recovered',
+  'cards/untracked-rate',
+  'cards/infection-status-by-national-index',
+  'cards/number-of-mutant-confirmed-cases',
+  'cards/number-of-inspection-persons',
+  'cards/number-of-reports-to-covid19-telephone-advisory-center',
+  'cards/number-of-tested',
+  'cards/number-of-mutant-inspection-persons',
+  'cards/vaccination-1st',
+  'cards/vaccination-2nd',
 ]
 
 options = webdriver.ChromeOptions()
@@ -36,11 +36,11 @@ driver = webdriver.Chrome(options=options)
 def screenshot(path, lang):
   driver.set_window_size(959,502)
   driver.get(
-    "http://localhost:8000{}/?ogp=true".format(
-      path if lang == "ja" else "/{}{}".format(lang, path)
+    "http://localhost:8000/{}/?ogp=true".format(
+      path if lang == "ja" else "{}/{}".format(lang, path)
     )
   )
-  path = path.replace("/cards/", "").replace("/", "_")
+  path = path.replace("cards/", "").replace("/", "_")
   driver.save_screenshot(
     "ogp/{}.png".format(
       path if lang == "ja" else "{}/{}".format(lang, path)
