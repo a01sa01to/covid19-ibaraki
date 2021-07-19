aa278195dc5e98a7f5b370526aa2fc5be48e219f
a01sa01to <a01sa01to@users.noreply.github.com>
2021-07-19 08:19:05 +0000

    deploy: 62eb923a4572f55f9481fd2b67af74991631da34

---
+++ _nuxt/static/1626682736/cards/ibaraki-graphical-map/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626682736",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcards\u002Fibaraki-graphical-map",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
