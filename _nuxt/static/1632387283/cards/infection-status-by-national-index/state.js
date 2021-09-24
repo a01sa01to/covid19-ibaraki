8a88376ce0c13e9d83985b1d5e7774bb25e828e3
a01sa01to <a01sa01to@users.noreply.github.com>
2021-09-24 09:13:57 +0000

    deploy: 61ec22475ba9a0a7f1d54c96f3dcb027430c08c9

--- _nuxt/static/1632387283/cards/infection-status-by-national-index/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632387283",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcards\u002Finfection-status-by-national-index",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
