cf8ad04f4a318e0fd60a4bd3f06ac5f92f882574
a01sa01to <a01sa01to@users.noreply.github.com>
2021-10-23 07:59:05 +0000

    deploy: 42f610bca8fa4cd337d65bc87cab19e07a3b5b2b

---
+++ _nuxt/static/1634975935/cards/infection-medical-care-provision-status/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634975935",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcards\u002Finfection-medical-care-provision-status",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
