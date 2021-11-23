0f57692858ef1bee5f7676e5cf411019192d9371
a01sa01to <a01sa01to@users.noreply.github.com>
2021-11-23 08:04:49 +0000

    deploy: ef63b13e92e0bed34045aef530491f511a404f3b

--- _nuxt/static/1637569315/cards/details-of-confirmed-cases/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637569315",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcards\u002Fdetails-of-confirmed-cases",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
