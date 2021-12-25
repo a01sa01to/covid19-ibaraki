6a0d0d32f1a8e52a7849237b55822c9a3f72e677
a01sa01to <a01sa01to@users.noreply.github.com>
2021-12-25 08:15:07 +0000

    deploy: a212c8fa24a872dc0cddebd763a41a1f21e8d52b

---
+++ _nuxt/static/1640420096/cards/number-of-reports-to-covid19-telephone-advisory-center/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640420096",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcards\u002Fnumber-of-reports-to-covid19-telephone-advisory-center",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
