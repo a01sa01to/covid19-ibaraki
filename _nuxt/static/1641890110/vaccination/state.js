f1c590360486eb09f55adfc023001066fc2fa42e
a01sa01to <a01sa01to@users.noreply.github.com>
2022-01-11 08:38:45 +0000

    deploy: b50ae772cb5f6972dc5bb82f24dc3e8a0031b20a

--- _nuxt/static/1641890110/vaccination/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641890110",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvaccination",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
