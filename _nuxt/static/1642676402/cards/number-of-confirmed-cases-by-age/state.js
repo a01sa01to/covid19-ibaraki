6b0680136eef1ad3fc73a765edabbdf229cf220d
a01sa01to <a01sa01to@users.noreply.github.com>
2022-01-21 09:44:50 +0000

    deploy: 70f341f0fc6e6556a9e5715c11376bb96ee8a30d

--- _nuxt/static/1642676402/cards/number-of-confirmed-cases-by-age/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642676402",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcards\u002Fnumber-of-confirmed-cases-by-age",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
