30b11d2246bf1f3964e85cf7a0a83471a3fa33da
a01sa01to <a01sa01to@users.noreply.github.com>
2022-02-17 07:24:20 +0000

    deploy: 7f2e4f53df762044bb299fb3e16fb7f52f9d2844

---
+++ _nuxt/static/1645082650/inspection/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645082650",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspection",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
