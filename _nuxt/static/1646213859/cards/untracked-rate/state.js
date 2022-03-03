ec26b8819ceab25ec726d232bc04d5004d9a5c58
a01sa01to <a01sa01to@users.noreply.github.com>
2022-03-03 10:22:52 +0000

    deploy: 3cc7a307982090a9dc071e6be7aaa5bc92f4d291

--- _nuxt/static/1646213859/cards/untracked-rate/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646213859",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcards\u002Funtracked-rate",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},__i18n:{langs:{}}}}(null));
